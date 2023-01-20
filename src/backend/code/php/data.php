6af81a5c97fa1d84f7c242aa160e6c8b8af7c5ac
aryan-11 <aryan11mobile@gmail.com>
2023-01-20 22:00:03 +0100

    start

---
+++ src/backend/code/php/data.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>data</title>
+    <link rel="stylesheet" href="../../../frontend/style.css">
+</head>
+<body>
+    <section class="container_page">
+        <nav><img src="../../../../assets/logo.png" alt="logo"></nav>
+        <div class="container_content">
+            <div class="container_piatti">
+                <div class="box_piatto">    
+                    <img src="../../../../assets/piatti/cozze.jpeg" alt="pasta cozze">
+                    <div class="infoPrezzi">
+                        <input type="hidden" value="1">
+                        <p>Pasta con cozze</p>
+                        <p>90 €</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container_tavoli">
+                <div class="nTavolo tavolo1">1</div>
+                <div class="nTavolo tavolo2">2</div>
+                <div class="nTavolo tavolo3">3</div>
+                <div class="nTavolo tavolo4">4</div>
+                <div class="nTavolo tavolo5">5</div>
+            </div>
+            <div class="container_resoconto">
+                <div class="resoconto_tavolo resoconto_tavolo1 hide">--- TAVOLO 1 ---</div>
+                <div class="resoconto_tavolo resoconto_tavolo2 hide">--- TAVOLO 2 ---</div>
+                <div class="resoconto_tavolo resoconto_tavolo3 hide">--- TAVOLO 3 ---</div>
+                <div class="resoconto_tavolo resoconto_tavolo4 hide">--- TAVOLO 4 ---</div>
+                <div class="resoconto_tavolo resoconto_tavolo5 hide">--- TAVOLO 5 ---</div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
