c8afd808e61432d877befb4648f3fec2b00cf4bd
aryan-11 <aryan11mobile@gmail.com>
2023-01-31 19:53:55 +0100

    read and write

--- src/backend/code/php/data.php
+++ src/backend/code/php/data.php
@@ -7,7 +7,16 @@
     <title>data</title>
     <link rel="stylesheet" href="../../../frontend/data.css">
     <link rel="stylesheet" href="../../../frontend/style.css">
-    <?php include "index.php"; ?>
+    <?php 
+          include "index.php";
+          $pietanze_csv = getfood();
+          
+          $tav = $_GET['tav'];
+          $nome = $_GET['nome'];
+          $prezzo = $_GET['prezzo'];
+          $flag = $_GET['flag'];
+          $azzera = $_GET['azzera'];
+    ?>
 </head>
 <body>
     <section class="container_page">
@@ -18,105 +27,85 @@
         </nav>
         <div class="container_content">
             <div class="container_piatti">
-                
-                <a href="#"><div class="box_piatto">    
-                        <div class="img_mask"><img id="piatto1" src="../../../../assets/piatti/cozze.jpeg" alt="pasta cozze"></div>
-                        <div class="infoPrezzi">
-                            <input type="hidden" value="1">
-                            <span class="nome">Pasta con cozze ~ </span>
-                            <span class="prezzo">90 €</span>
-                        </div>
-                    </div>
-                </a>
 
-                <!--  -->
-                <!--  -->
-                <a href="#"><div class="box_piatto">    
-                        <div class="img_mask"><img id="piatto1" src="../../../../assets/piatti/cozze.jpeg" alt="pasta cozze"></div>
-                        <div class="infoPrezzi">
-                            <input type="hidden" value="1">
-                            <span class="nome">Pasta con cozze ~ </span>
-                            <span class="prezzo">90 €</span>
-                        </div>
+
+
+            <?php foreach ($pietanze_csv as $index => $pietanza) { ?>
+                <a href="data.php?tav=<?= $tav; ?>&nome=<?= $pietanza[0]; ?>&prezzo=<?= $pietanza[1]; ?>&flag=1">
+                    <div class="box_piatto">
+                    <div class="img_mask">
+                        <img id="piatto<?= $index + 1; ?>" src="../../../../assets/piatti/<?= $pietanza[2]; ?>" alt="pasta cozze">
+                    </div>
+                    <div class="infoPrezzi">
+                        <input type="hidden" value="<?= $index + 1; ?>">
+                        <span class="nome"><?= $pietanza[0]; ?> ~ </span>
+                        <span class="prezzo"><?= $pietanza[1]; ?> €</span>
+                    </div>
                     </div>
                 </a>
-                <a href="#"><div class="box_piatto">    
-                        <div class="img_mask"><img id="piatto1" src="../../../../assets/piatti/cozze.jpeg" alt="pasta cozze"></div>
-                        <div class="infoPrezzi">
-                            <input type="hidden" value="1">
-                            <span class="nome">Pasta con cozze ~ </span>
-                            <span class="prezzo">90 €</span>
-                        </div>
-                    </div>
-                </a>
-                <a href="#"><div class="box_piatto">    
-                        <div class="img_mask"><img id="piatto1" src="../../../../assets/piatti/cozze.jpeg" alt="pasta cozze"></div>
-                        <div class="infoPrezzi">
-                            <input type="hidden" value="1">
-                            <span class="nome">Pasta con cozze ~ </span>
-                            <span class="prezzo">90 €</span>
-                        </div>
-                    </div>
-                </a>
-                <a href="#"><div class="box_piatto">    
-                        <div class="img_mask"><img id="piatto1" src="../../../../assets/piatti/cozze.jpeg" alt="pasta cozze"></div>
-                        <div class="infoPrezzi">
-                            <input type="hidden" value="1">
-                            <span class="nome">Pasta con cozze ~ </span>
-                            <span class="prezzo">90 €</span>
-                        </div>
-                    </div>
-                </a>
-                <a href="#"><div class="box_piatto">    
-                        <div class="img_mask"><img id="piatto1" src="../../../../assets/piatti/cozze.jpeg" alt="pasta cozze"></div>
-                        <div class="infoPrezzi">
-                            <input type="hidden" value="1">
-                            <span class="nome">Pasta con cozze ~ </span>
-                            <span class="prezzo">90 €</span>
-                        </div>
-                    </div>
-                </a>
-               
-                
+            <?php } ?>
 
 
             </div>
             <div class="container_left">
                 <div class="container_tavoli">
                     <p>Tavoli:</p>
-                    <div class="nTavolo tavolo1">1</div>
-                    <div class="nTavolo tavolo2">2</div>
-                    <div class="nTavolo tavolo3">3</div>
-                    <div class="nTavolo tavolo4">4</div>
-                    <div class="nTavolo tavolo5">5</div>
+                    <a href="data.php?tav=1&flag=0"><div class="nTavolo tavolo1">1</div></a>
+                    <a href="data.php?tav=2&flag=0"><div class="nTavolo tavolo2">2</div></a>
+                    <a href="data.php?tav=3&flag=0"><div class="nTavolo tavolo3">3</div></a>
+                    <a href="data.php?tav=4&flag=0"><div class="nTavolo tavolo4">4</div></a>
+                    <a href="data.php?tav=5&flag=0"><div class="nTavolo tavolo5">5</div></a>
                 </div>
                 <div class="container_resoconto">
                     <div class="resoconto_tavolo resoconto_tavolo1 ">
-                        <span class="numero_tavolo">Tavolo 1</span>
-                        <div class="ordine">
-                            <span class="nome_piatto">Pasta con cozze</span>
-                            <span class="prezzo_piatto">90</span>
-                        </div>
-                        <!--  -->
-                        <div class="ordine">
-                            <span class="nome_piatto">Pasta</span>
-                            <span class="prezzo_piatto">90</span>
-                        </div>
                         
-                        <div class="ordine">
-                            <span class="nome_piatto"><?php test(); ?></span>
-                            <span class="prezzo_piatto">90</span>
+                        <?php
+                              writeOrder($tav, $nome, $prezzo, $flag);
+                              $ordini = readOrder($tav);
+
+                            //   echo $ordini[0][0];
+                            //   echo $ordini[0][1];
+                        ?>
+                        <span class="numero_tavolo">Tavolo <?php echo $tav; ?></span>
+                        <div class="ordini">
+                            <?php
+                            foreach ($ordini as $ordine) {
+                                echo '<div class="ordine">';
+                                echo '<span class="nome_piatto">'.$ordine[0].'</span>';
+                                echo '<span class="prezzo_piatto">'.$ordine[1].'</span>';
+                                echo '</div>';
+                            }
+                            ?>
                         </div>
+
+                        <!-- <div class="ordine">
+                            <span class="nome_piatto"><?php echo $ordini[0][0];?></span>
+                            <span class="prezzo_piatto"><?php echo $ordini[0][1];?></span>
+                        </div>
+                        <div class="ordine">
+                            <span class="nome_piatto"><?php echo $ordini[1][0];?></span>
+                            <span class="prezzo_piatto"><?php echo $ordini[1][1];?></span>
+                        </div>
+                        <div class="ordine">
+                            <span class="nome_piatto"><?php echo $ordini[2][0];?></span>
+                            <span class="prezzo_piatto"><?php echo $ordini[2][1];?></span>
+                        </div> -->
+                        
+                        <!--  -->
+                        
                     </div>
 
-                    <div class="resoconto_tavolo resoconto_tavolo2 hide">--- TAVOLO 2 ---</div>
-                    <div class="resoconto_tavolo resoconto_tavolo3 hide">--- TAVOLO 3 ---</div>
-                    <div class="resoconto_tavolo resoconto_tavolo4 hide">--- TAVOLO 4 ---</div>
-                    <div class="resoconto_tavolo resoconto_tavolo5 hide">--- TAVOLO 5 ---</div>
+
                     <div class="resoconto">
                         <div class="totale_tavolo">0 €</div>
                         <div class="chiudiPaga_btn">Chiudi il conto e paga</div>
-                        <div class="Azzera">Azzera ordini</div>
+                        <a href="data.php?azzera=1"><div class="Azzera">Azzera tutti gli ordini</div>
+                        <?php 
+                            if($azzera == 1) {
+                                clearTableOrders();
+                                $azzera = 0;
+                            }
+                        ?>
                     </div>
                 </div>
             </div>
--- src/backend/code/php/index.php
+++ src/backend/code/php/index.php
@@ -1,17 +1,72 @@
 <?php
-    
+    // ini_set('display_errors', 1);
     function test() {
         echo 'la funzione';
     }
+    
+    function getFood() {
+        $temp=fopen("../../csv/pietanze.csv", "r") or die("Not Open");
+        $pietanze_csv=array();
+        $row = 1;
+    
+        if($temp !== FALSE){
+            while(($data=fgetcsv($temp, 1000, ","))!==FALSE){
+                $num=count($data);
+                $row++;
+                for($c=0; $c<$num; $c++){
+                }
+                $pietanze_csv[]=$data;
+            }
+            fclose($temp);
+        }
+
+        return $pietanze_csv;
+      }
 
     function writeOrder($nTavolo, $nome, $prezzo, $flag) {
         
-        if(($nTavolo = 1 || $nTavolo = 2|| $nTavolo = 3|| $nTavolo = 4|| $nTavolo = 5) && $flag == 1)
-        $filename = "tavolo".$nTavolo.".css";
-        $file = fopen($filename, 'a') or die("file opening error");
+        if (($nTavolo == 1 || $nTavolo == 2 || $nTavolo == 3 || $nTavolo == 4 || $nTavolo == 5) && $flag == 1) {
+          $filename = "tavolo".$nTavolo.".csv";
+          $file = fopen($filename, 'a') or die("file opening error in writing");
+          
 
-        fwrite($file, $nome.";".$prezzo.";\n");
-        fclose($file);
+          fwrite($file, $nome.";");
+          fwrite($file, $prezzo.";\n");
+          fclose($file);
+
+          $flag = 0;
+        }
+    }
+    
+    function readOrder($nTavolo) {
+        if (($nTavolo == 1 || $nTavolo == 2 || $nTavolo == 3 || $nTavolo == 4 || $nTavolo == 5)) {
+            $filename = "tavolo".$nTavolo.".csv";
+            $file = fopen($filename, 'r') or die("file opening error in read");
+            
+            $ordini=array();
+            $row = 1;
+            if($file !== FALSE){
+                while(($data=fgetcsv($file, 1000, ";"))!==FALSE){
+                    $num=count($data);
+                    $row++;
+                    for($c=0; $c<$num; $c++){
+                    }
+                    $ordini[]=$data;
+                }
+                fclose($file);
+            }
+            return $ordini;
+        }
+    }
+
+    function clearTableOrders() {
+        for($i=1; $i<=5; $i++) {
+            $filename = "tavolo".$i.".csv";
+            if (file_exists($filename)) {
+                $file = fopen($filename, 'w') or die("file opening error in clearing");
+                fclose($file);
+            }
+        }
     }
 
 
